99ef69c3a505900fa8b7077246410951f625d151
Ray0Emma <ahmadifarah22@gmail.com>
2022-03-19 20:51:15 +0000

    feat: added bootstrap

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,8 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <title>PHP PDO!</title>
-    {{-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script> --}}
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 
 <body data-bs-spy="scroll" data-bs-target="#navbar" class="min-vh-100">
@@ -23,6 +22,7 @@
     <!-- Footer section -->
     @include('components.footer')
     <!-- End Footer section -->
+    <script src="{{ mix('js/app.js') }}"></script>
 </body>
 
 </html>
